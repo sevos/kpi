bb115fc03aceb9db116217db95a750b896ea49c0
Artur Roszczyk <artur.roszczyk@gmail.com>
2011-02-21 00:37:21 +0100

    Not requiring files

--- lib/kpi.rb
+++ lib/kpi.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/entry')
-require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/report/suppress_memoization')
-require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/report/base')
-require File.join(File.dirname(__FILE__), '..', 'app/mailers/kpi/mailer')
+# require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/entry')
+# require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/report/suppress_memoization')
+# require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/report/base')
+# require File.join(File.dirname(__FILE__), '..', 'app/mailers/kpi/mailer')
