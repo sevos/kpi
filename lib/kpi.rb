b2b640c472029df987a72f40e1cea885749a0ae7
Artur Roszczyk <artur.roszczyk@gmail.com>
2011-02-22 12:55:48 +0100

    Refactoring Configuration

--- lib/kpi.rb
+++ lib/kpi.rb
@@ -1,16 +1,5 @@
 module KPI
   require 'engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 
-  mattr_accessor :app_name
-  @@app_name = "Your app"
-
-  mattr_accessor :mail_from
-  @@mail_from = "example@example.com"
-
-  mattr_accessor :mail_to
-  @@mail_to = "example@example.com"
-
-  def self.configure
-    yield self
-  end
+  include KPI::Configuration
 end
