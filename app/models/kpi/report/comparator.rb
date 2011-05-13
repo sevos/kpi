d7ab68cdd5c5b3ceb0a333251f9ee0d167f41ce7
Oskar Szrajer <oskarszrajer@gmail.com>
2011-05-13 14:24:21 +0200

    New methods in comperator

--- app/models/kpi/report/comparator.rb
+++ app/models/kpi/report/comparator.rb
@@ -4,26 +4,34 @@ module KPI
       extend KPI::Report::SuppressMemoization
       extend ActiveSupport::Memoizable
 
-      include KPI::Report::DynamicDefinitions
-      
-      blacklist :initialize, :average, :title
-
-      def initialize(*args)
-        @comparator ||= []
+      def initialize(*args, &block)
+        raise ArgumentError, "Should have any argument" if args.length == 0
+        raise Exception unless block_given?
+        raise ArgumentError, "Argument must be the same type" unless args.map(&:class).uniq.size == 1
         
-        args.each do |arg|
-          @comparator << arg 
-        end
-
+        @comparator ||= args
+        @compare = block
       end
-
-      def average
-        @comparator.instance_eval { reduce(:+) / size.to_f }
+      
+      def entries
+        Enumerator.new do |yielder|
+          @comparator.first.class.defined_kpis.each do |kpi_method|
+            result = @compare.call(*@comparator.map(&:"#{kpi_method}"))
+            yielder.yield(KPI::Entry.new(*result))
+          end
+        end
       end
 
       def title
         self.class.name
       end
+
+      def method_missing(name, *args)
+        @comparator.first.class.defined_kpis.each do |kpi_method|
+          @compare.call(*@comparator.map(&:"#{kpi_method}"))
+        end
+      end
+
     end
   end
 end
