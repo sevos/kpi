949c2a02819c9c56bfa2f21fa128fd038c0a76ea
Artur Roszczyk <artur.roszczyk@gmail.com>
2011-05-15 10:57:07 +0200

    Preload all classes at gem initialization

--- lib/kpi.rb
+++ lib/kpi.rb
@@ -3,4 +3,10 @@ require 'array/extract_options'
 module KPI
   require 'engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
   require 'kpi/18compatibility' if RUBY_VERSION < '1.9'
+  
+  require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/entry')
+  require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/report/dynamic_definitions')
+  require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/report/suppress_memoization')
+  require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/report')
+  require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/merged_report')
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,11 +9,3 @@ require 'kpi'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 # Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
-require File.join(File.dirname(__FILE__), '..', 'lib/kpi/configuration')
-require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/entry')
-require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/report/dynamic_definitions')
-require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/report/suppress_memoization')
-require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/report')
-require File.join(File.dirname(__FILE__), '..', 'app/models/kpi/merged_report')
-
