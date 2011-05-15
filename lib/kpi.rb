990e72816edd4d003918597badd6c3cea85ee59c
Artur Roszczyk <artur.roszczyk@gmail.com>
2011-05-15 10:54:07 +0200

    KPI gem doesn't require configuration now

--- lib/kpi.rb
+++ lib/kpi.rb
@@ -3,7 +3,4 @@ require 'array/extract_options'
 module KPI
   require 'engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
   require 'kpi/18compatibility' if RUBY_VERSION < '1.9'
-
-  require 'kpi/configuration'
-  include KPI::Configuration
 end
